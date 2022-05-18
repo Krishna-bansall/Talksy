eb5654a5a2016723fb79799f034a220d95cff2d8
Krishna Bansal <bansalkrishna321go@gmail.com>
2022-05-18 18:21:45 +0530

    test

---
+++ .history/components/Layout_20220518182126.tsx
@@ -0,0 +1,16 @@
+import React, { FC, ReactNode } from 'react'
+import { Navbar } from './Navbar'
+
+const Layout: FC<ReactNode> = ({ children }) => {
+  return (
+    <div className="flex flex-col justify-between bg-black text-white">
+      <Navbar />
+      <div className="overflow-hidden">
+        <main>{children}</main>
+        <footer>Footer</footer>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
