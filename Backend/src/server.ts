9e7e5949ef2e03bcbc508d4564c8fba6fbe139c5
Krishna Bansal <bansalkrishna321go@gmail.com>
2022-05-21 16:55:10 +0530

    Basic Express Server Set-UP

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+app.get("/", (req, res) => {
+	res.send("Hello From Express");
+});
+
+app.listen(PORT, () => {
+	console.log(`Listening on port:${PORT}`);
+});
